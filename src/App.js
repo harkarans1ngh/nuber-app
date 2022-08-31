f41e3b3cd8e0ee5c05294c0853676172433f1c83
Harkaran singh <56373545+pr0grmr@users.noreply.github.com>
2022-08-31 10:57:50 +0930

    Update App.js

--- src/App.js
+++ src/App.js
@@ -261,9 +261,9 @@ export default function App() {
     <Authenticator formFields={formFields} components={components}>
       {({ signOut }) => <button onClick={signOut}>Sign out</button>}
       
-      const user = await auth.currentAuthenticatedUser(); groupArray =
-      user.signInUserSession.accessToken.payload["cognito:groups"]
-      console.log(groupArray)
+      const user = await auth.currentAuthenticatedUser(); 
+      groupArray = user.signInUserSession.accessToken.payload["cognito:groups"];
+      console.log(groupArray);
       
     </Authenticator>
   );
